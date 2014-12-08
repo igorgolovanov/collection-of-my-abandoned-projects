a96a481503509c8234ca8de9266af165aee63dba
Igor Golovanov <igor.golovanov@kredito.de>
2014-12-08 18:29:16 +0200

    Zephir for ZF

--- php-src/ZephirExtra/Compiler.php
+++ php-src/ZephirExtra/Compiler.php
@@ -4,32 +4,53 @@ namespace ZephirExtra;
 
 class Compiler extends \Zephir\Compiler
 {
-    /**
-     * Pre-compiles classes creating a CompilerFile definition
-     *
-     * @param string $filePath
-     */
-    protected function preCompile($filePath)
-    {
-        if (preg_match('/\.zep$/', $filePath)) {
+	/**
+	 * Pre-compiles classes creating a CompilerFile definition
+	 *
+	 * @param string $filePath
+	 */
+	protected function preCompile($filePath)
+	{
+		if (preg_match('/\.zep$/', $filePath)) {
 
-            $className = str_replace('/', '\\', $filePath);
-            $className = preg_replace('/.zep$/', '', $className);
+			$className = str_replace('/', '\\', $filePath);
+			$className = preg_replace('/.zep$/', '', $className);
 
-            $className = join('\\', array_map(function ($i) {
-                return ucfirst($i);
-            }, explode('\\', $className)));
+			$className = join(
+				'\\', array_map(
+					function ($i) {
+						return ucfirst($i);
+					}, explode('\\', $className)
+				)
+			);
 
+			$this->files[$className] = new CompilerFile(
+				$className, $filePath, $this->config, $this->logger
+			);
+			$this->files[$className]->preCompile($this);
 
-            $this->files[$className] = new CompilerFile($className, $filePath, $this->config, $this->logger);
-            $this->files[$className]->preCompile($this);
+			$this->definitions[$className]
+				= $this->files[$className]->getClassDefinition();
+		}
+	}
 
-            $this->definitions[$className] = $this->files[$className]->getClassDefinition();
-        }
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function checkDirectory()
+	{
+		if (!$this->fileSystem->isInitialized()) {
+			$this->fileSystem->initialize();
+		}
 
-    protected function checkDirectory()
-    {
-        return 'zendframework';
-    }
+		if (!$this->fileSystem->exists(self::VERSION)) {
+			if (!$this->checkIfPhpized()) {
+				$this->logger->output(
+					'Zephir version has changed, use "zephir fullclean" to perform a full clean of the project'
+				);
+			}
+			$this->fileSystem->makeDirectory(self::VERSION);
+		}
+		return $this->getConfig()->get('namespace');
+	}
 } 
